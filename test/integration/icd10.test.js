65b67a4ffeef0be3809c418dc96f012efcf48fc1
maikcodes <whunter0111@gmail.com>
2023-08-22 18:54:44 -0500

    refactor application
    - change node 20.2.0 to 18.17.1 (fetch problem)
    - change test suite

--- test/integration/icd10.test.js
+++ test/integration/icd10.test.js
@@ -1,7 +1,8 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert'
 
-const BASE_URL = 'http://localhost:4000/api/v1/diseases'
+// const BASE_URL = 'http://localhost:4000/api/v1/diseases'
+const BASE_URL = 'https://api-icd10-dev.fl0.io/api/v1/diseases'
 
 describe('ICD-10 API endpoints suite test', async () => {
   it('Return json of diseases', async () => {
@@ -26,41 +27,6 @@ describe('ICD-10 API endpoints suite test', async () => {
       total_pages: 2135,
       data: [
         {
-          _id: '64ab35bc53aab3d22af99b40',
-          chapter_id: 'I',
-          chapter_title: 'I Certain infectious and parasitic diseases',
-          range_id: 'A00-A09',
-          range_title: 'A00-A09 Intestinal infectious diseases',
-          three_code_id: 'A00',
-          three_code_title: 'A00 Cholera',
-          four_code_id: 'A00.0',
-          four_code_title:
-            'A00.0 Cholera due to Vibrio cholerae 01, biovar cholerae'
-        },
-        {
-          _id: '64ab35bc53aab3d22af99b41',
-          chapter_id: 'I',
-          chapter_title: 'I Certain infectious and parasitic diseases',
-          range_id: 'A00-A09',
-          range_title: 'A00-A09 Intestinal infectious diseases',
-          three_code_id: 'A00',
-          three_code_title: 'A00 Cholera',
-          four_code_id: 'A00.1',
-          four_code_title:
-            'A00.1 Cholera due to Vibrio cholerae 01, biovar eltor'
-        },
-        {
-          _id: '64ab35bc53aab3d22af99b42',
-          chapter_id: 'I',
-          chapter_title: 'I Certain infectious and parasitic diseases',
-          range_id: 'A00-A09',
-          range_title: 'A00-A09 Intestinal infectious diseases',
-          three_code_id: 'A00',
-          three_code_title: 'A00 Cholera',
-          four_code_id: 'A00.9',
-          four_code_title: 'A00.9 Cholera, unspecified'
-        },
-        {
           _id: '64ab35bc53aab3d22af99b43',
           chapter_id: 'I',
           chapter_title: 'I Certain infectious and parasitic diseases',
@@ -72,21 +38,54 @@ describe('ICD-10 API endpoints suite test', async () => {
           four_code_title: 'A01.0 Typhoid fever'
         },
         {
-          _id: '64ab35bc53aab3d22af99b44',
+          _id: '64ab35bc53aab3d22af99b45',
           chapter_id: 'I',
           chapter_title: 'I Certain infectious and parasitic diseases',
           range_id: 'A00-A09',
           range_title: 'A00-A09 Intestinal infectious diseases',
           three_code_id: 'A01',
           three_code_title: 'A01 Typhoid and paratyphoid fevers',
-          four_code_id: 'A01.1',
-          four_code_title: 'A01.1 Paratyphoid fever A'
+          four_code_id: 'A01.2',
+          four_code_title: 'A01.2 Paratyphoid fever B'
+        },
+        {
+          _id: '64ab35bc53aab3d22af99b4a',
+          chapter_id: 'I',
+          chapter_title: 'I Certain infectious and parasitic diseases',
+          range_id: 'A00-A09',
+          range_title: 'A00-A09 Intestinal infectious diseases',
+          three_code_id: 'A02',
+          three_code_title: 'A02 Other salmonella infections',
+          four_code_id: 'A02.2',
+          four_code_title: 'A02.2 Localized salmonella infections'
+        },
+        {
+          _id: '64ab35bc53aab3d22af99b4b',
+          chapter_id: 'I',
+          chapter_title: 'I Certain infectious and parasitic diseases',
+          range_id: 'A00-A09',
+          range_title: 'A00-A09 Intestinal infectious diseases',
+          three_code_id: 'A02',
+          three_code_title: 'A02 Other salmonella infections',
+          four_code_id: 'A02.8',
+          four_code_title: 'A02.8 Other specified salmonella infections'
+        },
+        {
+          _id: '64ab35bc53aab3d22af99b4d',
+          chapter_id: 'I',
+          chapter_title: 'I Certain infectious and parasitic diseases',
+          range_id: 'A00-A09',
+          range_title: 'A00-A09 Intestinal infectious diseases',
+          three_code_id: 'A03',
+          three_code_title: 'A03 Shigellosis',
+          four_code_id: 'A03.0',
+          four_code_title: 'A03.0 Shigellosis due to Shigella dysenteriae'
         }
       ]
     }
 
     const result = await fetch(url).then((result) => result.json())
 
-    assert.deepStrictEqual(result, expectedResult, 'something is happening')
+    assert.deepStrictEqual(result, expectedResult)
   })
 })
