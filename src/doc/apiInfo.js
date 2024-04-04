9523169267646330d70745a57a309f383af516a0
Willy Maykros Romero <94816128+maikcodes@users.noreply.github.com>
2024-04-04 16:38:43 -0500

    Change app version in api docs

--- src/doc/apiInfo.js
+++ src/doc/apiInfo.js
@@ -1,6 +1,6 @@
 export const apiInfo = {
   title: 'ICD-10 API Documentation',
-  version: '2.0.0',
+  version: '2.1.0',
   description:
     "Integrate the ICD-10 microservice into your application to access the standard version of the World Health Organization's International Classification of Diseases (ICD-10)."
 }
